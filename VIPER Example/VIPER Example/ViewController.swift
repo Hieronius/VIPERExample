2c6ad986305341c26db20ab40bbe081f493c5fd2
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-12 14:28:33 +0300

    Create a new project

---
+++ VIPER Example/VIPER Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER Example
+//
+//  Created by Арсентий Халимовский on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
