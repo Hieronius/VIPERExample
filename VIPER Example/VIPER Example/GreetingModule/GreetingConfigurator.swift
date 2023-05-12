8e9e003d93e595796862806d566eebc4d73004e4
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-12 14:58:58 +0300

    Add Configurator

--- VIPER Example/VIPER Example/GreetingModule/GreetingConfigurator.swift
+++ VIPER Example/VIPER Example/GreetingModule/GreetingConfigurator.swift
@@ -5,4 +5,8 @@
 //  Created by Арсентий Халимовский on 12.05.2023.
 //
 
-import Foundation
+protocol GreetingConfiguratorInputProtocol {
+    // method to build the whole module in ViewController
+    // you should define type of ViewController, not a type of protocol
+    func configure(with view: GreetingViewController)
+}
