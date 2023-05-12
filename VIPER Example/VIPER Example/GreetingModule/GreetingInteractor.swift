bb1bcdd3c9162c3d09fe4796dfdbdaab76a04d47
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-12 15:17:21 +0300

    Define interactor

--- VIPER Example/VIPER Example/GreetingModule/GreetingInteractor.swift
+++ VIPER Example/VIPER Example/GreetingModule/GreetingInteractor.swift
@@ -5,16 +5,40 @@
 //  Created by Арсентий Халимовский on 12.05.2023.
 //
 
-protocol GreetingInteractorInputProtocol {
+protocol GreetingInteractorInputProtocol: AnyObject {
     // build Interactor for Presenter to use
     init(presenter: GreetingInteractorOutputProtocol)
     func provideGreetingData()
 }
 
-protocol GreetingInteractorOutputProtocol {
+protocol GreetingInteractorOutputProtocol: AnyObject {
     // special method to use only needed data from the Entity
     func receiveGreetingData(greetingData: GreetingData)
 }
 
-
+class GreetingInteractor: GreetingInteractorInputProtocol {
+    
+    // MARK: - Public Properties
+    
+    unowned let presenter: GreetingInteractorOutputProtocol
+    
+    
+    // MARK: - Inits
+    
+    required init(presenter: GreetingInteractorOutputProtocol) {
+        self.presenter = presenter
+    }
+    
+    // MARK: - Public Methods
+    
+    func provideGreetingData() {
+        // get data from original Model
+        let person = Person(name: "Tim", surname: "Cook")
+        let greetingData = GreetingData(name: person.name, surname: person.surname)
+        presenter.receiveGreetingData(greetingData: greetingData)
+        
+    }
+    
+    
+}
 
