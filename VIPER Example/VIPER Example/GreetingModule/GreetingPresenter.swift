aa72d7a85ab33e3952513f295fa0ea5af767f073
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-12 14:47:56 +0300

    Add Presenter

--- VIPER Example/VIPER Example/GreetingModule/GreetingPresenter.swift
+++ VIPER Example/VIPER Example/GreetingModule/GreetingPresenter.swift
@@ -5,4 +5,10 @@
 //  Created by Арсентий Халимовский on 12.05.2023.
 //
 
-import Foundation
+// Current state of the Model
+// Only required data for correct work of the Presenter
+struct GreetingData {
+    let name: String
+    let surname: String
+}
+
